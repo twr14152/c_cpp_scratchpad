fdf8da9ad1ea0b0389b8b4661690bce51d1a0d91
twr14152 <twr14152@gmail.com>
2019-07-20 15:48:54 +0000

    new file

---
+++ array_forloop.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void main(void)
+{
+  int a[10];
+  int count;
+  int i;
+  for (count = 0; count < 10; count++)
+  {
+    a[count] = count * 10 + count;
+  }
+  printf ("The first and third elements of a are %d and %d\n", 
+	a[0], a[1]);
+  printf ("Or, as pointers, %d and %d\n", *a, *(a+1));
+  printf ("The value of a[count] is %d\n", a[count]); 
+
+  for (i = 0; i < 10; i++)
+	{
+	printf ("a[%d] is equal to %d\n",i, a[i]);
+   }
+}
