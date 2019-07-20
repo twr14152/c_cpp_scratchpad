db5a876a5c9a189059326daaedd5980a139882c9
twr14152 <twr14152@gmail.com>
2019-07-20 18:04:36 +0000

    example of strlen func

---
+++ strlength.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+
+void main (void)
+{
+  char str1[30] = "This is 27 characters long.";
+  
+  printf("The length of the string '%s' is %d\n", str1, strlen (str1));
+}
+  
