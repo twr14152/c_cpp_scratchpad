d9a7b0545ca1ab0d43fd6c1024086f16904ed501
twr14152 <twr14152@gmail.com>
2019-07-23 15:29:12 +0000

    added headers

--- typical_CtoF_conv.c
+++ typical_CtoF_conv.c
@@ -4,15 +4,18 @@ main()
 {
 	float fahr, celsius;
 	int lower, upper, step;
+        char f[] = "fahr"; 
+        char c[] = "celcius";
 
 	lower = 0;
 	upper = 300;
         step = 20;
 
  	fahr = lower;
+	printf("%s\t%s\n", f, c);
 	while (fahr <= upper) {
 		celsius = (5.0/9.0) * (fahr - 32.0);
-		printf("%3.0f %6.1f\n", fahr, celsius);
+		printf("%3.0f\t %6.1f\n", fahr, celsius);
 		fahr = fahr + step;
 	}
 }
