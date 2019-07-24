f5ae8700b0ab56ce6543678fc9ddd77af5551004
twr14152 <twr14152@gmail.com>
2019-07-24 12:29:31 +0000

    Updated script with start and end marker

--- copy_in_out.c
+++ copy_in_out.c
@@ -5,11 +5,12 @@
 main()
 {
 	int c;
-
+        printf("*** Start of Script ***\n");
 	c = getchar();
 	while (c != EOF) {
 		putchar(c);
 		c = getchar();
 	}
+	printf("*** End of Script ***\n");
 }
 	
