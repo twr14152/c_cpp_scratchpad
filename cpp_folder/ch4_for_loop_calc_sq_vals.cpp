d8fb6b2eac65094c6b66bdccc3a20629360c89af
Todd Riemenschneider <twr14152@gmail.com>
2020-02-22 12:03:30 -0500

    Create ch4_for_loop_calc_sq_vals.cpp

---
+++ cpp_folder/ch4_for_loop_calc_sq_vals.cpp
@@ -0,0 +1,12 @@
+// Using for loops to calculate squared values 
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() 
+{
+	for (int i = 0; i <100; ++i) {
+		cout << i << '\t' << i*i << '\n';
+	}
+}
