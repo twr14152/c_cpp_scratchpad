be2063d1c60acdd80c8ae256d7e5316df5f7f0a8
Todd Riemenschneider <twr14152@gmail.com>
2020-02-22 13:00:56 -0500

    Create ch4_basic_func.cpp

---
+++ cpp_folder/ch4_basic_func.cpp
@@ -0,0 +1,16 @@
+//This example is how to write a basic func with return value
+//
+#include <iostream>
+
+
+using namespace std;
+
+int square(int x)
+{
+	return x * x;
+}
+
+int main()
+{
+	cout << "The value of square(10) is: " << square(10) << endl;
+}
