5c983ef77a0147364ddf66363b8a6549b6f4acd4
Todd Riemenschneider <twr14152@gmail.com>
2020-02-22 21:14:53 -0500

    Create ch4_vector_for_loop_append.cpp

---
+++ cpp_folder/ch4_vector_for_loop_append.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() 
+{
+	vector<string>v = {"red", "blue", "green", "black", "pink", "yellow"};
+	for (string x: v) // This is for each item x in v
+		cout << x << ",";
+		cout << "\n";
+
+        cout << "Now adding orange to the vector\n";	
+	v.push_back("orange");
+	for (string x: v)
+		cout << x << ",";
+	cout << "\n";
+	return 0;
+}
