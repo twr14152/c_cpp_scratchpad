7519cb6b010c1794644505585eb2dce5e6226f8f
Todd Riemenschneider <twr14152@gmail.com>
2020-05-25 11:27:47 -0400

    Create multiply_var.cpp

---
+++ cpp_folder/multiply_var.cpp
@@ -0,0 +1,12 @@
+// multiply var a * 3
+//
+#include <iostream>
+
+int main()
+{
+	int a {0};
+	std::cout << "Enter a number: ";
+	std::cin >> a;
+	std::cout << "3 x " << a << " = "  << a * 3 << '\n';
+	return 0;
+}
