aafe47a9b02f95567a261e02b10e490846706227
Todd Riemenschneider <twr14152@gmail.com>
2020-05-25 11:33:10 -0400

    Update multiply_var.cpp

--- cpp_folder/multiply_var.cpp
+++ cpp_folder/multiply_var.cpp
@@ -1,4 +1,4 @@
-// multiply var a * 3
+// multiply var a * 2 and a * 3
 //
 #include <iostream>
 
@@ -7,6 +7,13 @@ int main()
 	int a {0};
 	std::cout << "Enter a number: ";
 	std::cin >> a;
+	std::cout << "2 x " << a << " = " << a * 2 << '\n';
 	std::cout << "3 x " << a << " = "  << a * 3 << '\n';
 	return 0;
 }
+
+/* Results
+Enter a number: 567
+2 x 567 = 1134
+3 x 567 = 1701
+*/
