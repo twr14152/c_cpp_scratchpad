4a42d2cbc697895f69841f5ef9c14d3a5bda8bfe
Todd Riemenschneider <twr14152@gmail.com>
2020-05-25 08:36:28 -0400

    Update var_assignment.cpp

--- cpp_folder/var_assignment.cpp
+++ cpp_folder/var_assignment.cpp
@@ -1,5 +1,7 @@
-// Variable instantiation and assignment
-//
+// Variable initialization and assignment
+// Initialization - obj created and given a value upon definition
+// Assignment - obj is given a known value after the point of definition
+// Uninitualized - obj hasnt been given a known value yet
 //
 #include <iostream>
 
@@ -9,9 +11,21 @@ int main()
 	int x{4}; //Preferred method
 	int y(5);
 	int z = 10;
+	int u;
+
 	std::cout << "x = " << x << '\n';
 	std::cout << "y = " << y << '\n';
 	std::cout << "z = " << z << '\n';
 	std::cout << "x + y + z = "  << x + y + z << '\n';
+	std::cout << "u = " << u << '\n';
 	return 0;
 }
+
+/*
+x = 4
+y = 5
+z = 10
+x + y + z = 19
+u = 67044
+*/
+
