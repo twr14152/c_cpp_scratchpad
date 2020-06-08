1bc32486d525b9e064cd586f2fdff9ca915c2306
Todd Riemenschneider <twr14152@gmail.com>
2020-06-08 07:03:43 -0400

    Create hello.c

---
+++ tcpl/hello.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+main() 
+{
+	printf("Hello World!\n");
+}
+
+/*
+Just following example.....
+pi@RaspPi4:~/Coding/C_folder $ gcc -o hello hello.c
+hello.c:3:1: warning: return type defaults to 'int' [-Wimplicit-int]
+ main()
+ ^~~~
+pi@RaspPi4:~/Coding/C_folder $ ./hello
+Hello World!
+pi@RaspPi4:~/Coding/C_folder $ 
+*/
