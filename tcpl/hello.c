4061e0e8851b3156fcdcd6c3f4deb662931a1c21
Todd Riemenschneider <twr14152@gmail.com>
2020-06-08 07:55:13 -0400

    Update hello.c

--- tcpl/hello.c
+++ tcpl/hello.c
@@ -6,11 +6,6 @@ main()
 }
 
 /*
-Just following example.....
-pi@RaspPi4:~/Coding/C_folder $ gcc -o hello hello.c
-hello.c:3:1: warning: return type defaults to 'int' [-Wimplicit-int]
- main()
- ^~~~
 pi@RaspPi4:~/Coding/C_folder $ ./hello
 Hello World!
 pi@RaspPi4:~/Coding/C_folder $ 
