39d9f07025c5f77dd9f2b07091c6736cfafdca8a
Todd Riemenschneider <twr14152@gmail.com>
2020-06-08 08:27:29 -0400

    Update hello.c

--- tcpl/hello.c
+++ tcpl/hello.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
-main() 
+int main() 
 {
 	printf("Hello World!\n");
+	return 0;
 }
 
 /*
-pi@RaspPi4:~/Coding/C_folder $ ./hello
+pi@RaspPi4:~/Coding/C_folder $ gcc -o hello hello.c
+pi@RaspPi4:~/Coding/C_folder $ ./hello 
 Hello World!
 pi@RaspPi4:~/Coding/C_folder $ 
 */
