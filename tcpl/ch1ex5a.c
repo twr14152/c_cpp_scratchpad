e8e49436953c8bd38858dd409c7f22377cb57bd6
Todd Riemenschneider <twr14152@gmail.com>
2020-06-09 07:21:59 -0400

    Update ch1ex5a.c

--- tcpl/ch1ex5a.c
+++ tcpl/ch1ex5a.c
@@ -17,7 +17,7 @@ int main()
     printf("Table: Fahrenheit to Celsius\n");
     for (fahr = upper; fahr >= 0; fahr -= 20) 
         printf("%3.0f %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
-	return 0;
+    return 0;
 }
 /*
 pi@RaspPi4:~/Coding/C_folder $ ./ch1ex5 
