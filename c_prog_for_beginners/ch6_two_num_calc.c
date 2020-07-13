4f276c37036727302aaea44476f14b6e91d786b4
Todd Riemenschneider <twr14152@gmail.com>
2020-07-13 07:58:08 -0400

    Create ch6_two_num_calc.c

---
+++ c_prog_for_beginners/ch6_two_num_calc.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int main()
+{
+	int num1, num2;
+	char ov;
+	
+	printf("Enter math operator ('+'), ('-'), ('*'), ('/'), ('%'): ");
+	scanf("%c", &ov);
+	printf("This is a two number calculator\n");
+	printf("Enter number 1: ");
+	scanf("%d", &num1);
+	printf("Enter number 2: ");
+	scanf("%d", &num2);
+
+	switch(ov)
+	{
+	case '+':
+		printf("%d %c %d = %d\n", num1, ov, num2, num1 + num2);
+		break;
+	case '-':
+		printf("%d %c %d = %d\n", num1, ov, num2, num1 - num2);
+		break;
+	case '*':
+		printf("%d %c %d = %d\n", num1, ov, num2, num1 * num2);
+		break;
+	case '/':
+		if (num1 == 0 || num2 == 0)
+			printf("Cannot use 0 in division");
+		else 
+			printf("%d %c %d = %d\n", num1, ov, num2, num1/num2);
+		break;
+	case '%':
+		if (num1 == 0 || num2 == 0)
+			printf("Cannot use 0 in division\n\n\n");
+		else 
+			printf("%d %c %d = %d\n", num1, ov, num2, num1%num2);
+		break;
+	default:
+		printf("You entered something outside of parameters, ERROR\n\n\n");
+	}
+	return 0;
+}
