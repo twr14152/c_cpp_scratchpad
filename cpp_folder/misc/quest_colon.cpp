c1bc8e8cf1a878455f1a134996f836f710a70294
Todd Riemenschneider <twr14152@gmail.com>
2020-07-16 19:08:29 -0400

    Create quest_colon.cpp
    Just testing out the ?: feature

---
+++ cpp_folder/misc/quest_colon.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int a = 2;
+	int b = 4;
+	int max;
+	max = (a>b) ? a: b;
+	cout << max << endl;
+}
+
+
+/*
+*
+*pi@RaspPi4:~/Coding/CPP_folder/Misc $ ./quest_colon 
+*4
+*pi@RaspPi4:~/Coding/CPP_folder/Misc $ 
+*/
