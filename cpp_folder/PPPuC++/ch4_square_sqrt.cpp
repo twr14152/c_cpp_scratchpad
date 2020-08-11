cabaddf6571743197fdafc7b6cf66e8e8f3a8cf1
Todd Riemenschneider <twr14152@gmail.com>
2020-08-11 19:07:57 -0400

    Update ch4_square_sqrt.cpp

--- cpp_folder/PPPuC++/ch4_square_sqrt.cpp
+++ cpp_folder/PPPuC++/ch4_square_sqrt.cpp
@@ -15,17 +15,18 @@ int main()
 
 
 /*
-pi@RaspPi4:~/Coding/CPP_folder/PPPuCPP $ ./ch4_squares 
-0	square: 0	sqrt: 0
-1	square: 1	sqrt: 1
-2	square: 4	sqrt: 1.41421
-3	square: 9	sqrt: 1.73205
-4	square: 16	sqrt: 2
-5	square: 25	sqrt: 2.23607
-6	square: 36	sqrt: 2.44949
-7	square: 49	sqrt: 2.64575
-8	square: 64	sqrt: 2.82843
-9	square: 81	sqrt: 3
-10	square: 100	sqrt: 3.16228
+pi@RaspPi4:~/Coding/CPP_folder/PPPuCPP $ ./ch4_square_sqrt 
+starting number: 0	square: 0	sqrt: 0
+starting number: 1	square: 1	sqrt: 1
+starting number: 2	square: 4	sqrt: 1.41421
+starting number: 3	square: 9	sqrt: 1.73205
+starting number: 4	square: 16	sqrt: 2
+starting number: 5	square: 25	sqrt: 2.23607
+starting number: 6	square: 36	sqrt: 2.44949
+starting number: 7	square: 49	sqrt: 2.64575
+starting number: 8	square: 64	sqrt: 2.82843
+starting number: 9	square: 81	sqrt: 3
+starting number: 10	square: 100	sqrt: 3.16228
+
 <cropped>
 */
