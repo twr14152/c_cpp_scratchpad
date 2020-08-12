521133ece4b2fb63e8dac8f3103d416575cb3dc0
Todd Riemenschneider <twr14152@gmail.com>
2020-08-11 20:20:02 -0400

    Update ch4_drill_2.cpp

--- cpp_folder/PPPuC++/ch4_drill_2.cpp
+++ cpp_folder/PPPuC++/ch4_drill_2.cpp
@@ -15,9 +15,9 @@ int main() {
 		cin >> c;
 		cout << "a: " << a << " b: " << b << endl;
 		if (a > b)
-			cout << b << " is smaller than " << a << endl;
+			cout << "The smaller value is: " << b << "\nThe larger value is: " << a << endl;
 		else
-			cout << a << " is smaller than " << b << endl;
+			cout << "The smaller value is: " << a << "\nThe larger value is:  " << b << endl;
 		if (c == '|') {
 			cout << "Break seq entered" << endl;
 			break;
@@ -29,10 +29,11 @@ int main() {
 
 /*
 pi@RaspPi4:~/Coding/CPP_folder/PPPuCPP $ ./ch4_drill_2
-Enter an int value for a: 2
-Enter an int value for b: 4
+Enter an int value for a: 4
+Enter an int value for b: 6
 Enter a char value for c ('|' to break loop): |
-a: 2 b: 4
-2 is smaller than 4
+a: 4 b: 6
+The smaller value is: 4
+The larger value is:  6
 Break seq entered
 */
