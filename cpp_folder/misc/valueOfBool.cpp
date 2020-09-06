ffeb93def257da5cd382e93fb3f657c35a4fd256
Todd Riemenschneider <twr14152@gmail.com>
2020-09-06 10:41:26 -0400

    Create valueOfBool.cpp

---
+++ cpp_folder/misc/valueOfBool.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	bool booltrue = true;
+	bool boolfalse = false;
+	cout << "Numeric value of true: " << booltrue << endl;
+	cout << "Numeric value of false: " << boolfalse << endl;
+	return 0;
+}
+
+/*
+$ ./valueOfBool 
+Numeric value of true: 1
+Numeric value of false: 0 
+$ 
+*/
