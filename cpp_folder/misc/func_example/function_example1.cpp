a010198ceebf0136bb0da7a91080b03f0c7c3564
Todd Riemenschneider <twr14152@gmail.com>
2020-09-07 19:17:26 -0400

    Update function_example1.cpp

--- cpp_folder/misc/func_example/function_example1.cpp
+++ cpp_folder/misc/func_example/function_example1.cpp
@@ -14,3 +14,13 @@ int main()
 	cout << j << endl;
 	return 0;
 }
+
+
+/*
+pi@RaspPi4:~/Coding/CPP_folder/microsoft_CPP_tut $ g++ -std=c++17 function_example1.cpp Utilities.cpp -o function_example1   
+pi@RaspPi4:~/Coding/CPP_folder/microsoft_CPP_tut $ ./function_example1 
+5
+7
+9
+pi@RaspPi4:~/Coding/CPP_folder/microsoft_CPP_tut $ 
+*/
