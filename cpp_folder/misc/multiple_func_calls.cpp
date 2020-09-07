afb9ed0a7f30fe5567fbb50d72296304b5cd1905
Todd Riemenschneider <twr14152@gmail.com>
2020-09-07 17:30:04 -0400

    Create multiple_func_calls.cpp

---
+++ cpp_folder/misc/multiple_func_calls.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+void one();
+void two();
+
+int main() 
+{
+    cout << "main function" << endl;
+    one();
+    return 0;
+}
+
+void one() 
+{
+    cout << "function one" << endl;
+    two  ();
+}
+
+void two() 
+{
+    cout << "function two" << endl;
+}
