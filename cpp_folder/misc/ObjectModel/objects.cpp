9da159236809ba25a3398623abdf9d50c8c419f1
Todd Riemenschneider <twr14152@gmail.com>
2020-09-08 07:27:04 -0400

    Create objects.cpp

---
+++ cpp_folder/misc/ObjectModel/objects.cpp
@@ -0,0 +1,28 @@
+#include "Rectangle.h"
+#include <iostream>
+
+int main()
+{
+//	int x;
+//	int y{};
+//	int z{1};
+
+	Rectangle unitialized;   // do not do...
+	Rectangle value{};	 // preffered over above
+	Rectangle aggregate{ 4, 7 };
+	Rectangle const blue{ 4, 9 };  // make it a constant value
+	value.resize(5, 5);
+	aggregate.resize(1, 2);
+
+	int area_of_value{ value.area() };
+	int area_of_aggregate{ aggregate.area() };
+	int area_of_blue{ blue.get_area() };
+
+	//value._width = 2;
+	//value._height = 3;
+	
+	std::cout << "The area of rectangle aggregate is: " << aggregate.area() << std::endl;
+	std::cout << "The area of rectangle value is: " << value.area() << std::endl;
+	std::cout << "The area of rectangle blue is: " << blue.get_area() << std::endl;
+	return 0;
+}
