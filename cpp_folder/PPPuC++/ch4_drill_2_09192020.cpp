c629bf2634480ae8b927c2bb74d4115f5c3a091f
Todd Riemenschneider <twr14152@gmail.com>
2020-09-19 11:56:28 -0400

    Update ch4_drill_2_09192020.cpp

--- cpp_folder/PPPuC++/ch4_drill_2_09192020.cpp
+++ cpp_folder/PPPuC++/ch4_drill_2_09192020.cpp
@@ -16,7 +16,7 @@ int main()
       cout << "The larger number is: " << a << endl;
     } else {
       cout << "the smaller number is:" << a << endl;
-      cout << "the lager number is:" << b << endl;
+      cout << "the larger number is:" << b << endl;
     }
   }
   cout << endl;
@@ -28,7 +28,7 @@ int main()
 1 2
 a = 1 b = 2
 the smaller number is:1
-the lager number is:2
+the larger number is:2
 2 1
 a = 2 b = 1
 the smaller number is:1
@@ -36,7 +36,7 @@ The larger number is: 2
 3 8
 a = 3 b = 8
 the smaller number is:3
-the lager number is:8
+the larger number is:8
 8 3
 a = 8 b = 3
 the smaller number is:3
