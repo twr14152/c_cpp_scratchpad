f34d3c65095ab5501569e6d003bd804d791ca4cd
Todd Riemenschneider <twr14152@gmail.com>
2020-09-19 09:28:47 -0400

    Create ch4_vector_iteration.cpp

---
+++ cpp_folder/PPPuC++/ch4_vector_iteration.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+vector<int>ages={11, 13, 46, 46};
+vector<string>names{"Todd", "Lindsey", "Chuck", "Buck"};
+
+
+int main()
+{
+  for (int x : ages) {
+    cout <<"[" << x << "]";
+  }
+  cout << endl;
+
+  /*for (int i = 0; i < names.size(); i++) {
+    cout << "[" << names[i] << "]";
+  */
+  for (string i: names) {
+    cout << "[" << i << "]";
+  }
+  cout << endl;
+  return 0;
+}
