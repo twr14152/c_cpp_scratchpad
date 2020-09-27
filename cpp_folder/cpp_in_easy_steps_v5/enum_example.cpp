adb6d05b57a0f3b78605e3becf5ed7aff61720ff
Todd Riemenschneider <twr14152@gmail.com>
2020-09-27 09:58:17 -0400

    Create enum_example.cpp

---
+++ cpp_folder/cpp_in_easy_steps_v5/enum_example.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+
+int main() 
+{
+  const double PI = 3.1415926536;
+  cout << "6\" circle circumference:" << (PI*6) << endl;
+  enum { RED=1, YELLOW, GREEN, BROWN, BLUE, PINK, BLACK };
+  cout << "I shot a red worth: " << RED << endl;
+  cout << "Then a blue worth: " << BLUE << endl;
+  cout << "Total score: " << (RED + BLUE) << endl;
+
+  typedef enum { NEGATIVE, POSITIVE } charge;
+  charge neutral = NEGATIVE, live = POSITIVE;
+  cout << "Neutral wire: " << neutral << endl;
+  cout << "Live wire: " << live << endl;
+  return 0;
+}
+
+/*
+./main
+6" circle circumference:18.8496
+I shot a red worth: 1
+Then a blue worth: 5
+Total score: 6
+Neutral wire: 0
+Live wire: 1
+*/
