2ae50ba9c248b28b14adb9dc26cb5b46952bde68
Todd Riemenschneider <twr14152@gmail.com>
2020-09-29 06:38:49 -0400

    Create try_except_01.cpp

---
+++ cpp_folder/cpp_in_easy_steps_v5/try_except_01.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int number;
+  try 
+  { 
+    for (number = 1; number < 21; number ++)
+    {
+      if (number > 10) throw (number);
+      else 
+      {
+        cout << "Number: " << number << endl;
+      }
+    }
+  }
+  catch(int num)
+  { 
+    cout << "Exception at: " << num << endl; 
+  }
+  return 0;
+}
+
+/*
+ ./main
+Number: 1
+Number: 2
+Number: 3
+Number: 4
+Number: 5
+Number: 6
+Number: 7
+Number: 8
+Number: 9
+Number: 10
+Exception at: 11
+ 
+*/
