a506f1affd8caf2174cd228293fdf48d46d0aa75
Todd Riemenschneider <twr14152@gmail.com>
2020-10-01 08:27:29 -0400

    Create object.cpp

---
+++ cpp_folder/cpp_in_easy_steps_v5/object.cpp
@@ -0,0 +1,50 @@
+// Create an Object
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Dog
+{
+int age, weight;
+string color;
+public:
+void setAge(int years)
+{
+  age = years;
+}
+void setWeight(int lbs)
+{
+  weight = lbs;
+}
+void setColor(string n)
+{
+  color = n;
+}
+void talk() {cout << "Woof "<< endl;}
+int getAge() {return age;}
+int getWeight() {return weight;}
+string getColor() {return color;}
+};
+
+
+int main()
+{
+  Dog jack;
+  jack.setAge(6);
+  jack.setWeight(140);
+  jack.setColor("brown");
+  cout << "Jack is a " << jack.getColor() << " dog" << endl;
+  cout << "Jack is " << jack.getAge() << " years old" << endl;
+  cout << "Jack weighs " << jack.getWeight() << " pounds" << endl;
+  jack.talk();
+  return 0;
+}
+
+/*
+ ./main
+Jack is a brown dog
+Jack is 6 years old
+Jack weighs 140 pounds
+Woof 
+
+*/
