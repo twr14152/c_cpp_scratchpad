a31ccebf013a4ae0291ab155a4f8de4e07b08a1b
Todd Riemenschneider <twr14152@gmail.com>
2020-10-02 07:26:59 -0400

    Create multiple_objects.cpp

---
+++ cpp_folder/cpp_in_easy_steps_v5/multiple_objects.cpp
@@ -0,0 +1,44 @@
+// Create an Object
+// access specifiers { private = default, public, protected }
+//
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Dog
+{
+	int age, weight;
+	string color;
+	
+	public:
+	void setValues( int, int, string);
+	void bark() {cout << "Woof "<< endl;}
+	int getAge() {return age;}
+	int getWeight() {return weight;}
+	string getColor() {return color;}
+};
+void Dog::setValues(int age, int weight, string color)
+{
+	this -> age = age;
+	this -> weight = weight;
+	this -> color = color;
+}
+
+
+int main()
+{
+  Dog jack;
+  jack.setValues(6, 140, "brown");
+  cout << "Jack is a " << jack.getColor() << " dog" << endl;
+  cout << "Jack is " << jack.getAge() << " years old" << endl;
+  cout << "Jack weighs " << jack.getWeight() << " pounds" << endl;
+  jack.bark();
+  return 0;
+}
+/*
+pi@RaspPi4:~/Coding/CPP_folder/in_easy_steps $ ./multiple_objects 
+Jack is a brown dog
+Jack is 6 years old
+Jack weighs 140 pounds
+Woof 
+*/
