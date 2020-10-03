41231f9c813b2640bc67adb11474edd17b6cdcc2
Todd Riemenschneider <twr14152@gmail.com>
2020-10-03 09:15:31 -0400

    Create override.cpp

---
+++ cpp_folder/cpp_in_easy_steps_v5/override.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Man
+{
+	//
+	public:
+		void speak() {cout << "Hello!" << endl;}
+		void speak(string msg){cout << "(Base Class) " << msg << endl;}
+
+	//
+};
+class Hombre : public Man
+{
+	public:
+		void speak(string msg){cout << "(child class)" << msg << endl;}
+
+};
+
+int main()
+{
+	Man henry;
+	Hombre enrique;
+	henry.speak();
+	henry.speak("I wonder which method will be used ???");
+	enrique.speak("Hola!");
+	enrique.Man::speak("Que pasa mi amigo?");
+	return 0;
+}
+pi@RaspPi4:~/Coding/CPP_folder/in_easy_steps $ ./override 
+Hello!
+(Base Class) I wonder which method will be used ???
+(child class)Hola!
+(Base Class) Que pasa mi amigo?
+pi@RaspPi4:~/Coding/CPP_folder/in_easy_steps $ 
