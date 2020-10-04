96cc867119787f94da127a7475f12e7162e5f2ef
Todd Riemenschneider <twr14152@gmail.com>
2020-10-04 10:27:05 -0400

    Create adt.cpp

---
+++ cpp_folder/cpp_in_easy_steps_v5/adt.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+using namespace std;
+
+class Shape
+{
+	public:
+		virtual int getArea() = 0;
+		virtual int getEdge() = 0;
+		virtual void Draw() = 0;
+};
+
+class Rect : public Shape
+{
+	private:
+		int height, width;
+
+
+
+	public:
+		Rect( int initWidth, int initHeight )
+		{
+			height = initHeight;
+			width =  initWidth;
+		}
+		~Rect();
+
+		int getArea() { return height * width; };
+		int getEdge() { return(2 * height) + (2 * width);}
+
+		void Draw()
+		{
+			for (int i = 0; i < height ; i++){
+				for (int j = 0;j < width; j++){cout << "x ";}
+			       cout << endl;
+			}
+		}
+
+
+};
+
+int main()
+{
+	Shape* pQuad = new Rect(3,7);
+	Shape* pSquare = new Rect(5,5);
+	pQuad -> Draw();
+	cout << "Area is " << pQuad -> getArea() << endl;
+	cout << "Perimeter is " << pQuad -> getEdge() << endl;
+
+	pSquare -> Draw();
+	cout << "Area is " << pSquare -> getArea() << endl;
+	cout << "Perimeter is " << pSquare -> getEdge() << endl;
+
+	return 0;
+}
+
+/*
+pi@RaspPi4:~/Coding/CPP_folder/in_easy_steps $ ./adt 
+x x x 
+x x x 
+x x x 
+x x x 
+x x x 
+x x x 
+x x x 
+Area is 21
+Perimeter is 20
+x x x x x 
+x x x x x 
+x x x x x 
+x x x x x 
+x x x x x 
+Area is 25
+Perimeter is 20
+pi@RaspPi4:~/Coding/CPP_folder/in_easy_steps $ 
+*/
