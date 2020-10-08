abe1ac4bf085afac22a76a78921ab81293db5f07
Todd Riemenschneider <twr14152@gmail.com>
2020-10-07 22:39:14 -0400

    Create hello.c

---
+++ c_in_easy_steps/ch1/hello.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() 
+{
+	printf("Hello, mutha f***ers\n");
+	return 0;
+}
+
+
+
+
+/*
+pi@RaspPi4:~/Coding/C_folder/in_easy_steps/ch2 $ gcc hello.c -save-temps -o hello 
+
+pi@RaspPi4:~/Coding/C_folder/in_easy_steps/ch1 $ ls -l
+total 40
+-rwxr-xr-x 1 pi pi  7980 Oct  7 19:45 hello
+-rw-r--r-- 1 pi pi    82 Oct  7 19:45 hello.c
+-rw-r--r-- 1 pi pi 16828 Oct  7 19:45 hello.i
+-rw-r--r-- 1 pi pi  1036 Oct  7 19:45 hello.o
+-rw-r--r-- 1 pi pi   750 Oct  7 19:45 hello.s
+pi@RaspPi4:~/Coding/C_folder/in_easy_steps/ch1 $
+*/
