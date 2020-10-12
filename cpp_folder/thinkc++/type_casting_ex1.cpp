67e157908a434f6af5fa3f98435edd0358bdf3a4
Todd Riemenschneider <twr14152@gmail.com>
2020-10-12 15:36:50 -0400

    Update type_casting_ex1.cpp

--- cpp_folder/thinkc++/type_casting_ex1.cpp
+++ cpp_folder/thinkc++/type_casting_ex1.cpp
@@ -1,5 +1,6 @@
-// Typecasting from Think C++ book compared to other style
-// both appear to work. Neither are preferred. Recommended 'static_cast<type> variable'
+// Typecasting from Think C++ book compared to other old c-style
+// both appear to work however neither are the recommended method
+// learncpp.com recommends using 'static_cast<type>(var)' method
 #include <iostream>
 
 using namespace std;
@@ -9,20 +10,26 @@ int main()
 	double pi {3.14159};
 	cout << pi << endl;
 
-	int x = int (pi); //Think C++ had example i've not seen
+	int x = int (pi); //Think C++ had func like form of casting
 	cout << x << endl;	
 	
-	int y = (int) pi; //This is the fmt I've seen to read more learncpp.com
+	int y = (int) pi; //This is the fmt I've seen
 	cout << y << endl;
 	
+	int z = static_cast<int> (pi); // This is the preferred 
+	cout << z << endl;
+
 	cout << pi << endl;
 	return 0;
 }
+
 /*
 pi@RaspPi4:~/Coding/CPP_folder/thinkc++ $ ./type_cast_ex1 
 3.14159
 3
 3
+3
 3.14159
 pi@RaspPi4:~/Coding/CPP_folder/thinkc++ $ 
+ 
 */
