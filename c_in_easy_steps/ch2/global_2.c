b17d6a4994a5ec795b61938207c8116e0df10f57
Todd Riemenschneider <twr14152@gmail.com>
2020-10-14 07:41:10 -0400

    Update global_2.c

--- c_in_easy_steps/ch2/global_2.c
+++ c_in_easy_steps/ch2/global_2.c
@@ -1,3 +1,11 @@
-// If I would add 'static int num = 100' it would only be accessible via this file.
+/* If I would add 'static int num = 100' it would only be accessible via this file.
+   
+   static int num = 100
+
+   spi@RaspPi4:~/Coding/C_folder/in_easy_steps/ch2 $ gcc global_1.c global_2.c -o global
+   /usr/bin/ld: /tmp/cc2LjGb7.o: in function `main':
+   global_1.c:(.text+0x44): undefined reference to `num'
+   collect2: error: ld returned 1 exit status
+*/
 
 int num = 200;
