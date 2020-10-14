319c1ee65dfdf09d5763ee944b8b144225e92429
Todd Riemenschneider <twr14152@gmail.com>
2020-10-14 07:43:24 -0400

    Update global_2.c

--- c_in_easy_steps/ch2/global_2.c
+++ c_in_easy_steps/ch2/global_2.c
@@ -1,6 +1,6 @@
 /* If I would add 'static int num = 100' it would only be accessible via this file.
    
-   static int num = 100
+   static int num = 100;
 
    $ gcc global_1.c global_2.c -o global
    /usr/bin/ld: /tmp/cc2LjGb7.o: in function `main':
