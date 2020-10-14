24739234161a59068a942280f9e019067892517f
Todd Riemenschneider <twr14152@gmail.com>
2020-10-14 12:33:43 -0400

    Update cast.c

--- c_in_easy_steps/ch2/cast.c
+++ c_in_easy_steps/ch2/cast.c
@@ -4,14 +4,20 @@ int main()
 {
 	int x = 66;
 	char y = 'B';
+	float decimal = 6.78;
+	int num = 1;
+
 	printf("cast %d\n", x);
 	printf("x equals %c\n",(char)x);
 	printf("cast %c\n", y);
 	printf("y equals %d\n", (int)y);
 	printf("\n");
+	printf("Float %f\n", decimal);
+	printf("Converted into int %d\n", (int)decimal);
+	printf("Int %d\n", num);
+	printf("Converted into float %f\n",(float)num);
 	return 0;
 }
-
 /*
 $ ./cast 
 cast 66
@@ -19,5 +25,9 @@ x equals B
 cast B
 y equals 66
 
-$ 
+Float 6.780000
+Converted into int 6
+Int 1
+Converted into float 1.000000
+$
 */
