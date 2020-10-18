a1f17fe645261ae7ce9e3ca1eda41dbed2b62866
Todd Riemenschneider <twr14152@gmail.com>
2020-10-18 08:37:23 -0400

    Update ternary.c

--- c_in_easy_steps/ch4/ternary.c
+++ c_in_easy_steps/ch4/ternary.c
@@ -5,11 +5,15 @@
 int main(void) 
 {
 	(10 / 2 == 5) ? printf("correct\n") : printf("false\n");
+
+	//unexpected output expression result printed first
+	printf("using ternary exp in print statement\n", (3 == 4)? printf("true\n"): printf("false\n"));
 	return 0;
 }
-
 
 /*
 ./ternary
 correct
+false
+using ternary exp in print statement
 */
