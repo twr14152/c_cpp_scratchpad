9fc626e6d1e95224d4e9ac1369178ed2a042f3d1
Todd Riemenschneider <twr14152@gmail.com>
2020-10-19 23:36:26 +0000

    Create switch.c

---
+++ c_in_easy_steps/ch5/switch.c
@@ -0,0 +1,23 @@
+
+
+#include <stdio.h>
+
+int main(void) 
+{
+  int num = 2;
+  char letter ='b';
+  
+  switch(num)
+  {
+    case 1: printf("Number is one\n"); break;
+    case 2: printf("Number is two\n"); break;
+    case 3: printf("Number is three\n"); break;
+    default: printf("Number is unrecognized\n");
+  }
+  switch(letter)
+  {
+    case 'a': case 'b': case 'c':
+    printf("letter is %c\n", letter);break;
+    default: printf("letter is unrecognized\n");
+  }
+}
