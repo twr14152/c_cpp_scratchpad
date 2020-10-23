bbc842c5acaa1bec860b1fe84b5d4ca862060ca4
Todd Riemenschneider <twr14152@gmail.com>
2020-10-23 19:47:32 -0400

    Create hello.c

---
+++ effective_c/hello.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("Hello, world!");
+	return EXIT_SUCCESS;
+}
+/*
+pi@RaspPi4:~/Coding/C_folder/effective_c/ch1 $ ./hello 
+Hello, world!
+pi@RaspPi4:~/Coding/C_folder/effective_c/ch1 $ 
+*/
