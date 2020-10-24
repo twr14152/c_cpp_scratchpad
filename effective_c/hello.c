d91f71e2a50545581845221e03c3886853c5b320
Todd Riemenschneider <twr14152@gmail.com>
2020-10-23 20:33:31 -0400

    Update hello.c

--- effective_c/hello.c
+++ effective_c/hello.c
@@ -1,3 +1,12 @@
+/* According to this book printf("string\n") can open you up for 
+ * a security vulnerability and therefore you should use puts
+ * for strings to stdout
+ * printf("Hello world\n");
+ * should only be used for formating
+ * printf("%s\n", "hello world")
+ * Use puts("string\n") 
+ * The return value seems like more work than using (return 0;) 
+*/
 #include <stdio.h>
 #include <stdlib.h>
 
