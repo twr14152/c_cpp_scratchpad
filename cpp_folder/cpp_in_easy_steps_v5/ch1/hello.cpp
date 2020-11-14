ca2e3a52d8f66abf9c9581380211b525f518e46a
Todd Riemenschneider <twr14152@gmail.com>
2020-11-13 20:10:37 -0500

    Create hello.cpp

---
+++ cpp_folder/cpp_in_easy_steps_v5/ch1/hello.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Hello World!" << endl;
+	cout << "Hello World again!\n";
+	return 0;
+}
+/*
+pi@RaspPi4:~/Coding/CPP_folder/in_easy_steps/ch1 $ ./hello 
+Hello World!
+Hello World again!
+pi@RaspPi4:~/Coding/CPP_folder/in_easy_steps/ch1 $ 
+*/
