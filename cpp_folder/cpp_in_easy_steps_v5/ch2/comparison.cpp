9f5df12d593f437262670f615c71b6e681a33a18
Todd Riemenschneider <twr14152@gmail.com>
2021-03-16 08:11:53 -0400

    Create comparison.cpp

---
+++ cpp_folder/cpp_in_easy_steps_v5/ch2/comparison.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Equal 0 = false, 1 = true" <<endl;
+	cout << "Does 65 = A? " << "answer is = " << (65 == 'A') << endl;
+	cout << "Does 97 == A? " << "answer is = " << (96 == 'A') << endl;
+	return 0;
+}
+/*
+pi@RaspPi4:~/Coding/CPP_folder/in_easy_steps/ch2 $ ./comparison 
+Equal 0 = false, 1 = true
+Does 65 = A? answer is = 1
+Does 97 == A? answer is = 0
+pi@RaspPi4:~/Coding/CPP_folder/in_easy_steps/ch2 $ 
+*/
