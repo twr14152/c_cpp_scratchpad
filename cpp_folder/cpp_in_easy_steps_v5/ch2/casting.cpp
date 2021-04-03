62286756fd983e57f7d7fa9f25a7d79e819dcde9
Todd Riemenschneider <twr14152@gmail.com>
2021-04-03 15:54:00 -0400

    Create casting.cpp

---
+++ cpp_folder/cpp_in_easy_steps_v5/ch2/casting.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+ int num = 7, factor = 2;
+ char letter = 'A'; float result = 0.0;
+
+ cout << "Integer division: " << (num/factor) << endl;
+
+ result = (float)(num)/factor;
+ cout << "Cast division float: " << result << endl;
+
+ num = static_cast<int>(letter);
+ cout << "Cast character int: " << num << endl;
+
+ letter = static_cast<char>(70);
+ cout << "Cast integer char: " << letter << endl;
+  return 0;
+}
+
+/*
+ ./main
+Integer division: 3
+Cast division float: 3.5
+Cast character int: 65
+Cast integer char: F
+ 
+*/
