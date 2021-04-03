68078bd100edb5f2bdd1f6aad1cd42d28bdeeed5
Todd Riemenschneider <twr14152@gmail.com>
2021-04-03 10:21:23 -0400

    Update logic.cpp

--- cpp_folder/cpp_in_easy_steps_v5/ch2/logic.cpp
+++ cpp_folder/cpp_in_easy_steps_v5/ch2/logic.cpp
@@ -5,14 +5,19 @@ int main()
 {
   int a = 1, b = 0;
 
+  cout << "a = " << a << endl;
+  cout << "b = " << b << endl;
+
   cout << "AND logic:" << endl;
-  cout <<"(a && a)" << (a && a) << "(true)" << endl;
-  cout <<"(a && b)" << (a && b) << "(false)" << endl;
+  cout <<"(a && a) " << (a && a) << " (true)" << endl;
+  cout <<"(a && b) " << (a && b) << " (false)" << endl;
+  cout <<"(b && b) " << (b && b) << " (false)" << endl;
 
 
   cout << "OR logic:" << endl;
-  cout <<"(a || a)" << (a || a) << "(true)" << endl;
-  cout <<"(a || b)" << (a || b) << "(true)" << endl;
+  cout <<"(a || a) " << (a || a) << " (true)" << endl;
+  cout <<"(a || b) " << (a || b) << " (true)" << endl;
+  cout <<"(b || b) " << (b || b) << " (false)" << endl;
 
   cout << "NOT logic:" << endl;
   cout <<"a = " << a << " " << "!a = " << !a << endl;
@@ -21,13 +26,17 @@ int main()
 }
 
 /*
- ./logic
+ ./main
+a = 1
+b = 0
 AND logic:
-(a && a)1(true)
-(a && b)0(false)
+(a && a) 1 (true)
+(a && b) 0 (false)
+(b && b) 0 (false)
 OR logic:
-(a || a)1(true)
-(a || b)1(true)
+(a || a) 1 (true)
+(a || b) 1 (true)
+(b || b) 0 (false)
 NOT logic:
 a = 1 !a = 0
 b = 0 !b = 1
