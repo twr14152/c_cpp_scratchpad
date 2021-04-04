8efe02b5799c0df8e448bb8b0ad1e2cf3c56c73d
Todd Riemenschneider <twr14152@gmail.com>
2021-04-04 10:26:19 -0400

    Update for_loop.cpp

--- cpp_folder/cpp_in_easy_steps_v5/ch3/for_loop.cpp
+++ cpp_folder/cpp_in_easy_steps_v5/ch3/for_loop.cpp
@@ -6,11 +6,13 @@ int main()
   int i, j;
   for (i = 1; i < 4; i++)
   {
+    cout << "Outer loop start: " << i << endl;
     for (j = 1; j < 4; j++)
     {
       cout << "Inner loop iteration: " << j << endl;
     }
-    cout << "Outer loop: " << i << endl;
+    cout << "Outer loop end: " << i << endl;
+    cout << "=====================" << endl;
   }
 
   return 0;
@@ -18,17 +20,23 @@ int main()
 
 /*
  ./main
+Outer loop start: 1
 Inner loop iteration: 1
 Inner loop iteration: 2
 Inner loop iteration: 3
-Outer loop: 1
+Outer loop end: 1
+=====================
+Outer loop start: 2
 Inner loop iteration: 1
 Inner loop iteration: 2
 Inner loop iteration: 3
-Outer loop: 2
+Outer loop end: 2
+=====================
+Outer loop start: 3
 Inner loop iteration: 1
 Inner loop iteration: 2
 Inner loop iteration: 3
-Outer loop: 3
+Outer loop end: 3
+=====================
  
 */
