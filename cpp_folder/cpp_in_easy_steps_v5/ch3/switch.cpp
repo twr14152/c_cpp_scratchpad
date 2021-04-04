adaf9ecd31a064e6f290cfe2b1c225d182dcd20d
Todd Riemenschneider <twr14152@gmail.com>
2021-04-04 09:31:07 -0400

    Update switch.cpp

--- cpp_folder/cpp_in_easy_steps_v5/ch3/switch.cpp
+++ cpp_folder/cpp_in_easy_steps_v5/ch3/switch.cpp
@@ -12,7 +12,8 @@ int main()
     case 1 : cout << "Monday" << endl; break;
     case 2 : cout << "Tuesday" << endl; break;
     case 3 : cout << "Wednesday" << endl; break;case 4 : cout << "Thursday" << endl; break;
-    case 5 : cout << "Friday" << endl; break;default : cout << "Weekend day" << endl;
+    case 5 : cout << "Friday" << endl; break;
+    default : cout << "Weekend day" << endl;
   }
   return 0;
 }
