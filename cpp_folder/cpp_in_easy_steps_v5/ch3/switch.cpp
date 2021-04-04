6b93e288fae071ab1f05c02da1ccb4c88526c6b2
Todd Riemenschneider <twr14152@gmail.com>
2021-04-04 09:30:18 -0400

    Create switch.cpp

---
+++ cpp_folder/cpp_in_easy_steps_v5/ch3/switch.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int day = 0;
+  printf("Enter number 0-7 which day of the week is it (1 = Monday....7=Sunday):\n");
+  scanf("%d", &day);
+  
+  switch (day)
+  {
+    case 1 : cout << "Monday" << endl; break;
+    case 2 : cout << "Tuesday" << endl; break;
+    case 3 : cout << "Wednesday" << endl; break;case 4 : cout << "Thursday" << endl; break;
+    case 5 : cout << "Friday" << endl; break;default : cout << "Weekend day" << endl;
+  }
+  return 0;
+}
+
+/*
+ ./main
+Enter number 0-7 which day of the week is it (1 = Monday....7=Sunday):
+2
+Tuesday
+ ./main
+Enter number 0-7 which day of the week is it (1 = Monday....7=Sunday):
+5
+Friday
+ ./main
+Enter number 0-7 which day of the week is it (1 = Monday....7=Sunday):
+6
+Weekend day
+ 
+*/
