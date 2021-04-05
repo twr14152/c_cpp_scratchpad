52f896b1eb8ce9dd2a60883fe9b54826f19bfa4a
Todd Riemenschneider <twr14152@gmail.com>
2021-04-05 19:10:34 -0400

    Create optimize.cpp

---
+++ cpp_folder/cpp_in_easy_steps_v5/ch3/optimize.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+int computeFactorials(int, int);
+int factorial(int);
+
+int main() {
+	computeFactorials(1, 8);
+	return 0;
+}
+
+int computeFactorials(int num, int max) {
+	cout << "Factorials of " << num << ":";
+	cout << factorial(num) << endl;
+	num++;
+	if (num > max)
+		return 0;
+	else
+		computeFactorials(num, max);
+}
+
+int factorial(int n) {
+	int result;
+	if (n == 1)
+		result = 1;
+	else {
+		result = (factorial(n - 1) * n);
+		return result;
+	}
+}
+/*
+ ./main
+Factorials of 1:1
+Factorials of 2:2
+Factorials of 3:6
+Factorials of 4:24
+Factorials of 5:120
+Factorials of 6:720
+Factorials of 7:5040
+Factorials of 8:40320
+
+*/
