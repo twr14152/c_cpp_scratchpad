8f7b5c7e92610ad78e1745c3afc34cbf010fc3f1
Todd Riemenschneider <twr14152@gmail.com>
2021-06-14 07:50:05 -0400

    Update array_of_ptrs.c

--- Mod_C_for_Abs_Beg/pointers/array_of_ptrs.c
+++ Mod_C_for_Abs_Beg/pointers/array_of_ptrs.c
@@ -2,6 +2,12 @@
 
 int main(void)
 {
+	char* string_ptr = "Whats up!";
+	printf("%s", string_ptr); //%s prints out entire string
+ 	printf("\n");	
+	char* char_ptr = "Whats up!";
+	printf("%c", *char_ptr);  //%c prints out first character
+	printf("\n");
 	char *p[] = {"First sentence", "Second sentence", "Third sentence"};
 	for (int i = 0; i < 3; i++)
 	{
@@ -13,10 +19,11 @@ int main(void)
 
 /*
 pi@RaspPi4:~/Coding/C_folder/Modern_C_Absolute_beg $ ./array_of_ptrs 
+Whats up!
+W
 First sentence
 Second sentence
 Third sentence
 pi@RaspPi4:~/Coding/C_folder/Modern_C_Absolute_beg $ 
 */
-
 
