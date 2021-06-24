0e210ba54c695f6ff6f025314a9207d912e45406
Todd Riemenschneider <twr14152@gmail.com>
2021-06-24 07:24:48 -0400

    Create sscanf_example.c

---
+++ Mod_C_for_Abs_Beg/stdin_stdout/sscanf_example.c
@@ -0,0 +1,19 @@
+//sscanf uses buffered data instead of data from a standard input
+
+#include <stdio.h>
+
+int main(void)
+{
+	char buff[100] = "config t\nrouter bgp 65535\nnetwork 1.0.0.1 mask 255.255.255.255\n";
+	char c[100];
+	sscanf(buff, "%100c",&c);
+	printf("The value of buff is:\n%s", c);
+}
+/*
+pi@RaspPi4:~/Coding/C_folder/Modern_C_Absolute_beg $ ./sscanf_example 
+The value of buff is:
+config t
+router bgp 65535
+network 1.0.0.1 mask 255.255.255.255
+pi@RaspPi4:~/Coding/C_folder/Modern_C_Absolute_beg $ 
+*/
