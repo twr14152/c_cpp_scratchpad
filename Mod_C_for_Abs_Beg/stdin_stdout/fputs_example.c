d04d1597da6c231d133b465a2f0a095348799bcd
Todd Riemenschneider <twr14152@gmail.com>
2021-06-26 09:42:48 -0400

    Create fputs_example.c

---
+++ Mod_C_for_Abs_Beg/stdin_stdout/fputs_example.c
@@ -0,0 +1,18 @@
+//This is another means of handling string data but you can chose the output stream
+//
+#include <stdio.h>
+
+int main(void)
+{
+	fputs("fputs() prints string the same as puts()\n", stdout);
+    fputs("You need to add newline char\n", stdout);
+	fputs("You need to specify which output stream to send data to\n", stdout);
+}
+
+/*
+pi@RaspPi4:~/Coding/C_folder/Modern_C_Absolute_beg $ ./fputs_example 
+fputs() prints string the same as puts()
+You need to add newline char
+You need to specify which output stream to send data to
+pi@RaspPi4:~/Coding/C_folder/Modern_C_Absolute_beg $ 
+*/
