d05df65c61ed710aa10249e9c3a826eea8d6d7cb
Todd Riemenschneider <twr14152@gmail.com>
2021-06-26 09:26:08 -0400

    Create puts_example.c

---
+++ Mod_C_for_Abs_Beg/stdin_stdout/puts_example.c
@@ -0,0 +1,22 @@
+//puts() is an easy way to print string data
+//
+#include <stdio.h>
+
+int main(void)
+{
+	char str[] = "Sample string";
+	puts("Puts() is recommended to print strings");
+  puts("Puts() automatically adds '/nl' character");
+	puts("That is when string operators arent needed");
+	puts("");
+	puts(str);
+}
+
+/*
+Puts() is recommended to print strings
+Puts() automatically adds '/nl' character
+That is when string operators arent needed
+
+Sample string
+
+*/
