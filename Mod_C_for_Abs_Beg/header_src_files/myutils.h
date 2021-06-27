5c30d4f470be7289efa927917cdaf67dec5e20ce
Todd Riemenschneider <twr14152@gmail.com>
2021-06-27 09:29:27 -0400

    Update myutils.h

--- Mod_C_for_Abs_Beg/header_src_files/myutils.h
+++ Mod_C_for_Abs_Beg/header_src_files/myutils.h
@@ -1,3 +1,13 @@
+/* 
+The Preprocessor directives make it so this header file is included only once
+no matter how many src files call it during compilation.
+*/
+
+#ifndef MY_UTILS_H
+#define MY_UTILS_H
+
 #include <stdio.h>
 
 void myFunc();
+
+#endif
