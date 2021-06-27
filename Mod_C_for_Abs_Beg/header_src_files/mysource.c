f76b5d6f8c7278aa65853995584e77cee28e4a68
Todd Riemenschneider <twr14152@gmail.com>
2021-06-27 09:23:33 -0400

    Create mysource.c

---
+++ Mod_C_for_Abs_Beg/header_src_files/mysource.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "myutils.h"
+
+int main(void)
+{
+	myFunc();
+}
+
+
+/*
+pi@RaspPi4:~/Coding/C_folder/Modern_C_Absolute_beg $ gcc -Wall mysource.c myutils.c -std=c17 -o mysource
+pi@RaspPi4:~/Coding/C_folder/Modern_C_Absolute_beg $ ./mysource 
+Declared in .h file and defined in source .c file
+pi@RaspPi4:~/Coding/C_folder/Modern_C_Absolute_beg $ 
+*/
