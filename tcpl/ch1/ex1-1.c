9f4f6f2cac82811b6bf87d65fe0fe7eb2fe8a315
Todd Riemenschneider <twr14152@gmail.com>
2021-07-15 07:05:07 -0400

    Create ex1-1.c

---
+++ tcpl/ch1/ex1-1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("Hello,");
+	printf(" world");
+	printf("\n");
+}
+
+/*
+pi@RaspPi4:~/Coding/C_folder/TCPL/ch1 $ ./ex1-1  
+Hello, world
+pi@RaspPi4:~/Coding/C_folder/TCPL/ch1 $ 
+*/
