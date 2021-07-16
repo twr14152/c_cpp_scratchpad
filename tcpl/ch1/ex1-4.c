4a21eac716eef2d275deaa6a8ad1f6b832d7e1f9
Todd Riemenschneider <twr14152@gmail.com>
2021-07-16 05:51:32 -0400

    Update ex1-4.c

--- tcpl/ch1/ex1-4.c
+++ tcpl/ch1/ex1-4.c
@@ -1,19 +1,22 @@
 #include <stdio.h>
 
+#define LOWER 0
+#define UPPER 300
+#define STEP 20
+
 int main()
 {
 	puts("Celsius - Fahrenheit table");
 	float fahr, celsius;
-	for (fahr = 300; fahr >= 0; fahr -= 20) {
+	for (fahr = UPPER; fahr >= LOWER; fahr -= STEP) {
 		celsius = (5.0/9.0) * (fahr - 32.0);
 		//printf("%3.0f %6.1f\n", fahr, celsius);
 		printf("%6.1f %3.0f\n", celsius, fahr);
 	}
 }
-
 /*
 pi@RaspPi4:~/Coding/C_folder/TCPL/ch1 $ gcc -std=c17 ex1-4.c -o ex1-4
-pi@RaspPi4:~/Coding/C_folder/TCPL/ch1 $ ./ex1-4
+pi@RaspPi4:~/Coding/C_folder/TCPL/ch1 $ ./ex1-4 
 Celsius - Fahrenheit table
  148.9 300
  137.8 280
