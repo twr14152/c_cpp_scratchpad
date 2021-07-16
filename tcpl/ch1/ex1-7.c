3e9462cf709970185518ce695519b185089b82c6
Todd Riemenschneider <twr14152@gmail.com>
2021-07-15 20:20:44 -0400

    Create ex1-7.c

---
+++ tcpl/ch1/ex1-7.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("EOF = %d\n",EOF);
+}
+/*
+pi@RaspPi4:~/Coding/C_folder/TCPL/ch1 $ gcc -std=c17 ex1-7.c -o ex1-7
+pi@RaspPi4:~/Coding/C_folder/TCPL/ch1 $ ./ex1-7
+EOF = -1
+pi@RaspPi4:~/Coding/C_folder/TCPL/ch1 $ 
+*/
