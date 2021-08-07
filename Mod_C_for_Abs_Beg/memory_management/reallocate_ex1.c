db783b29435c37f3dc17127a64bb40d6a4f2eb39
Todd Riemenschneider <twr14152@gmail.com>
2021-08-07 08:22:42 -0400

    Create reallocate_ex1.c

---
+++ Mod_C_for_Abs_Beg/memory_management/reallocate_ex1.c
@@ -0,0 +1,21 @@
+// reallocate memory allocated by malloc or calloc
+// void *realloc(void *ptr_to_allocated_mem, size_t new_size_in_bytes)
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	int* p = malloc(sizeof(int));
+	printf("Allocated %zu bytes.\n", sizeof(*p));
+	printf("Resizing allocated memory..\n");
+	int* pnew = realloc(p, 10 * sizeof(int));
+	printf("The memory block is now %zu bytes long. \n", 10 * sizeof(int));
+}
+/*
+toddriemenschneider@Todds-MBP-2 ModC4AbsBeg % ./reallocate_ex1
+Allocated 4 bytes.
+Resizing allocated memory..
+The memory block is now 40 bytes long. 
+toddriemenschneider@Todds-MBP-2 ModC4AbsBeg % 
+*/
