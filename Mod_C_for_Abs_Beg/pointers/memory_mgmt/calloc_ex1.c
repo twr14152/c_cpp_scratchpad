09971660dc70e7875b0341a2970d25045f85bed6
Todd Riemenschneider <twr14152@gmail.com>
2021-08-07 08:04:34 -0400

    Create calloc_ex1.c

---
+++ Mod_C_for_Abs_Beg/pointers/memory_mgmt/calloc_ex1.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	int* p = calloc(5, sizeof(int));
+	if (p)
+	{
+		printf("Initial values: \n");
+		for (int i = 0; i < 5; i++)
+		{
+			printf("%d ", p[i]);
+		}
+		printf("\nNew values:\n");
+		for (int i = 0; i < 5; i++)
+		{
+			p[i] = (i + 1) * 10;
+			printf("%d ", p[i]);
+		}
+	}
+	free(p);
+	puts(" ");
+} 
+/*
+toddriemenschneider@Todds-MBP-2 ModC4AbsBeg % ./calloc_ex1                           
+Initial values: 
+0 0 0 0 0 
+New values:
+10 20 30 40 50  
+toddriemenschneider@Todds-MBP-2 ModC4AbsBeg % 
+*/
