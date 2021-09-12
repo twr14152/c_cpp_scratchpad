a697f166ba36c7f12bd8667d6ff3cf6fd16d6682
Todd Riemenschneider <twr14152@gmail.com>
2021-09-12 08:49:50 -0400

    Update access_character.cpp

--- cpp_folder/ModernC++4AbsBeg/strings/access_character.cpp
+++ cpp_folder/ModernC++4AbsBeg/strings/access_character.cpp
@@ -6,9 +6,9 @@ int main()
 	std::string s = "This is a string";
 	char c1 = s[0];
 	char c2 = s.at(1);
-	char c3 = s[3];
-	char c4 = s.at(4);
-	char c5 = s.at(5);
+	char c3 = s[2];
+	char c4 = s.at(3);
+	char c5 = s.at(4);
 	std::cout << "First characters: " << c1 << '\n';
 	std::cout << "Second char: " << c2 << std::endl;
 	std::cout << "Third char: " << c3 << std::endl;
@@ -16,13 +16,12 @@ int main()
 	std::cout << "Fifth char: " << c5 << std::endl;
 	return 0;
 }
-
 /*
 pi@RaspPi4:~/Coding/CPP_folder/Misc $ ./access_characters
 First characters: T
 Second char: h
-Third char: s
-Fourth char:  
-Fifth char: i
+Third char: i
+Fourth char: s
+Fifth char:  
 pi@RaspPi4:~/Coding/CPP_folder/Misc $ 
 */
