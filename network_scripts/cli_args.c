70920a20358bb2212fdaed480bab2212ec0cb944
Todd Riemenschneider <twr14152@gmail.com>
2021-10-24 08:30:56 -0400

    Update cli_args.c

--- network_scripts/cli_args.c
+++ network_scripts/cli_args.c
@@ -1,16 +1,22 @@
 #include <stdio.h>
 
-int main(int argc, char* argv[]) {
-	for (int i = 1; i < argc; i++)
+int main(int argc, char *argv[])
+{
+	for (int i = 1 ; i < argc; i++)
 	{
-		printf("This is command number %d command: %s\n", i ,  argv[i]);
+		printf("command %d : %s\n", i, argv[i]);
 	}
 	return 0;
 }
+
+
+
 /*
-pi@RaspPi4:~/Coding/C_folder/misc/ssh_stuff $ ./cli_args "config t" "interface eth0" "ip address 10.0.0.1/24"
-This is command number 1 command: config t
-This is command number 2 command: interface eth0
-This is command number 3 command: ip address 10.0.0.1/24
-pi@RaspPi4:~/Coding/C_folder/misc/ssh_stuff $ 
+pi@RaspPi4:~/Coding/C_folder/ssh_client_stuff/basic_ssh $ ./ssh_cli "cmh1-rtr1" "config t" "router ospf 1" "no auto-summary" "end" 
+command 1 : cmh1-rtr1
+command 2 : config t
+command 3 : router ospf 1
+command 4 : no auto-summary
+command 5 : end
+pi@RaspPi4:~/Coding/C_folder/ssh_client_stuff/basic_ssh $ 
 */
