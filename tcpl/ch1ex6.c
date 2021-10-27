e5764e712c93ef54c7384e9ea5d71c93d2737042
Todd Riemenschneider <twr14152@gmail.com>
2021-10-27 06:18:21 -0400

    Update ch1ex6.c

--- tcpl/ch1ex6.c
+++ tcpl/ch1ex6.c
@@ -1,5 +1,3 @@
-/* Verify the expression getchar() != EOF is 0 or 1 */
-
 #include <stdio.h>
 
 int main()
@@ -7,30 +5,15 @@ int main()
 	int c;
         /* The precedence of != is higher than = so () are necessary */
 	 while ((c = getchar()) != EOF) {
-		 printf("\n%d - ", c != EOF);
 		 putchar(c);
 
 	 }
-	printf("The value of c is %d\n", c != EOF);
-	printf("The value of EOF is %d\n", EOF);
 	return 0;
 }
 
+
 /*
-pi@RaspPi4:~/Coding/C_folder $ ./ch1ex6 < testfile
-
-1 - T
-1 - e
-1 - s
-1 - t
-1 -  
-1 - d
-1 - a
-1 - t
-1 - a
-1 - 
-The value of c is 0
-The value of EOF is -1
-pi@RaspPi4:~/Coding/C_folder $ 
+./main < test
+This is a test of char 0 and 1.
+To make sure they are not EOF values.
 */
-
