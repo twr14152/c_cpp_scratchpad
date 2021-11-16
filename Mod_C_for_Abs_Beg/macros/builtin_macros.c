11dd1421ea226b99a3c2e65cfb8f0a3d13e30cf2
Todd Riemenschneider <twr14152@gmail.com>
2021-11-16 05:38:25 -0500

    Create builtin_macros.c

---
+++ Mod_C_for_Abs_Beg/macros/builtin_macros.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+void test_func() {
+	printf("This is function is : %s\n", __func__);
+}
+
+int main(void) {
+  printf("This is line number: %d\n", __LINE__);
+  printf("File name: %s\n", __FILE__);
+  printf("Date created: %s\n", __DATE__);
+  printf("Your running c version: %ld\n", __STDC_VERSION__);
+  test_func();
+  return 0;
+}
+
+
+/*
+pi@RaspPi4:~/Coding/C_folder/Modern_C_Absolute_beg $ ./a.out
+This is line number: 8
+File name: builtin_macros.c
+Date created: Nov 16 2021
+Your running c version: 201710
+This is function is : test_func
+pi@RaspPi4:~/Coding/C_folder/Modern_C_Absolute_beg $ 
+*/
