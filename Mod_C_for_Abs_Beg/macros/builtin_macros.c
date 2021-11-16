a0543e922db3af2a27da18d684f72e6d92dcbf09
Todd Riemenschneider <twr14152@gmail.com>
2021-11-16 05:40:06 -0500

    Update builtin_macros.c

--- Mod_C_for_Abs_Beg/macros/builtin_macros.c
+++ Mod_C_for_Abs_Beg/macros/builtin_macros.c
@@ -6,18 +6,16 @@ void test_func() {
 
 int main(void) {
   printf("This is line number: %d\n", __LINE__);
-  printf("File name: %s\n", __FILE__);
+  printf("Source file name: %s\n", __FILE__);
   printf("Date created: %s\n", __DATE__);
   printf("Your running c version: %ld\n", __STDC_VERSION__);
   test_func();
   return 0;
 }
-
-
 /*
 pi@RaspPi4:~/Coding/C_folder/Modern_C_Absolute_beg $ ./a.out
 This is line number: 8
-File name: builtin_macros.c
+Source file name: builtin_macros.c
 Date created: Nov 16 2021
 Your running c version: 201710
 This is function is : test_func
