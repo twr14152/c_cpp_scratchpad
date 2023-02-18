6976539e1ae83e626024624b3776b333ebe5a23e
Todd Riemenschneider <twr14152@gmail.com>
2023-02-17 20:04:44 -0500

    Create ex1_5.c

---
+++ tcpl/ch1/ex1_5.c
@@ -0,0 +1,36 @@
+/*
+Exercise 1-5. Modify the temperature conversion program to print the table in
+reverse order, that is, from 300 degrees to O. 0
+*/
+
+#include <stdio.h>
+
+int main()
+{
+	puts("Fahrenheit - Celsius table");
+	float fahr, celsius;
+	for (fahr = 300; fahr >= 0; fahr -= 20) {
+		celsius = (5.0/9.0) * (fahr - 32.0);
+		printf("%3.0f %6.1f\n", fahr, celsius);
+	}
+}
+
+/*
+Fahrenheit - Celsius table
+300  148.9
+280  137.8
+260  126.7
+240  115.6
+220  104.4
+200   93.3
+180   82.2
+160   71.1
+140   60.0
+120   48.9
+100   37.8
+ 80   26.7
+ 60   15.6
+ 40    4.4
+ 20   -6.7
+  0  -17.8
+*/
