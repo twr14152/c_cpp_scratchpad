5936e5120bd896f7dca3ed458268ec087a6647d0
Todd Riemenschneider <twr14152@gmail.com>
2023-02-26 12:05:07 -0500

    Update ch3ex6.cpp

--- cpp_folder/PPPuC++/ch3/ch3ex6.cpp
+++ cpp_folder/PPPuC++/ch3/ch3ex6.cpp
@@ -10,67 +10,55 @@ int main()
 	cout << "Enter 3 number: ";
 	cin >> a >> b >> c;
 	if (a > b && a > c && b > c) {
-		cout << c << b << a << endl;
+		cout << c << "," << b << "," << a << endl;
 	}
 	else if ( a < b && a < c && b < c ) {
-		cout << a << b << c << endl;
+		cout << a << "," << b << "," << c << endl;
 	}
 	else if ( a < b && b > c && a < c) {
-		cout << a << c << b << endl;
+		cout << a << "," << c << "," << b << endl;
 	}
 	else if ( a < b && a > c ) {
-		cout << c << a << b << endl;
+		cout << c << "," << a << "," << b << endl;
 	}
 	else if ( a > b && b < c && a > c) {
-		cout << b << c << a << endl;
+		cout << b << "," << c << "," << a << endl;
 	}
 	else if ( a > b && b < c && c > a) {
-		cout << b << a << c << endl;
+		cout << b << "," << a << "," << c << endl;
 	}
 	else if ( a == b ) {
 		if ( a < c ) {
-			cout << b << a << c << endl;
+			cout << b << "," << a << "," << c << endl;
 		}
 		else {
-			cout << c << b << a << endl;
+			cout << c << "," << b << "," << a << endl;
 		}
 	}
 	else if ( a == c) {
 		if ( a < b ) {
-			cout << a << c << b << endl;
+			cout << a << "," << c << "," << b << endl;
 		}
 		else {
-			cout << b << a << c << endl;
+			cout << b << "," << a << "," << c << endl;
 		}
 	}
 	else if (b == c) {
 		if (a > b) {
-			cout << c << b << a << endl;
+			cout << c << "," << b << "," << a << endl;
 		}
 		else {
-			cout << a << c << b << endl;
+			cout << a << "," << c << "," << b << endl;
 		}
 	}
 	return 0;
 }
 /*
-pi@raspberrypi:~/Code_folder/cpp_folder/PPPv2/ch3 $ ./ch3ex6 
-Enter 3 number: 1 2 3
-123
-pi@raspberrypi:~/Code_folder/cpp_folder/PPPv2/ch3 $ ./ch3ex6 
-Enter 3 number: 1 3 2
-123
-pi@raspberrypi:~/Code_folder/cpp_folder/PPPv2/ch3 $ ./ch3ex6 
-Enter 3 number: 3 2 1
-123
-pi@raspberrypi:~/Code_folder/cpp_folder/PPPv2/ch3 $ ./ch3ex6 
-Enter 3 number: 1 1 2
-112
-pi@raspberrypi:~/Code_folder/cpp_folder/PPPv2/ch3 $ ./ch3ex6 
-Enter 3 number: 2 1 1
-112
-pi@raspberrypi:~/Code_folder/cpp_folder/PPPv2/ch3 $ ./ch3ex6 
-Enter 3 number: 1 2 1
-112
+pi@raspberrypi:~/Code_folder/cpp_folder/PPPv2/ch3 $ ./ch3ex6
+Enter 3 number: 10 4 6
+4,6,10
+pi@raspberrypi:~/Code_folder/cpp_folder/PPPv2/ch3 $ ./ch3ex6
+Enter 3 number: 4 5 4
+4,4,5
 pi@raspberrypi:~/Code_folder/cpp_folder/PPPv2/ch3 $ 
 */
