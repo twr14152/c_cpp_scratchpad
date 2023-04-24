88965dae6603bf48cd308b3303c3c42e6567a7e9
Todd Riemenschneider <twr14152@gmail.com>
2023-04-24 05:53:02 -0400

    Create longestline_c17.c

---
+++ tcpl/misc/longestline_c17.c
@@ -0,0 +1,56 @@
+/* This is a version of getline rwwritten to work with c17 */
+
+#include <stdio.h>
+#define MAXLINE 1000
+
+int longestline(char line[], int maxline);
+void copy(char to[], char from[]);
+
+int main(void)
+{
+	int len;
+	int max;
+	char line[MAXLINE];
+	char longest[MAXLINE];
+
+	max = 0;
+	while ((len = longestline(line, MAXLINE)) > 0)
+	{
+		if (len > max) {
+			max = len;
+			copy(longest, line);
+		}
+	}
+	
+	if (max > 0) {
+		printf("%s", longest);
+	}
+	return 0;
+}
+
+int longestline(char s[], int lim)
+{
+	int c, i;
+	for (i = 0; i<lim -1 && (c = getchar()) != EOF && c!='\n'; ++i)
+	{
+		s[i] = c;
+	}
+	if (c == '\n') {
+		s[i] = c;
+		++i;
+	}
+	s[i] = '\0';
+	return i;
+}
+
+void copy(char to[], char from[])
+{
+	int i;
+	i = 0;
+	while ((to[i] = from[i]) != '\0')
+	{
+		++i;
+	}
+}
+		
+		
