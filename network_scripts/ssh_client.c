8a059cbcf8d672692d285d036dc4a443b4fc8fa6
Todd Riemenschneider <twr14152@gmail.com>
2024-04-01 20:03:26 -0400

    Update ssh_client.c
    Cleaned up and included better example of functionality

--- network_scripts/ssh_client.c
+++ network_scripts/ssh_client.c
@@ -63,12 +63,12 @@ int main(int argc , char *argv[]) {
     printf("\nCommand: %s\n", argv[2]);    
     rc = ssh_channel_request_exec(channel, argv[2]);
     if (rc != SSH_OK) {
-	    fprintf(stderr, "Error executing command: %s\n", ssh_get_error(session));
-            ssh_channel_close(channel);
-            ssh_channel_free(channel);
-            ssh_disconnect(session);
-            ssh_free(session);
-            exit(EXIT_FAILURE);
+	fprintf(stderr, "Error executing command: %s\n", ssh_get_error(session));
+        ssh_channel_close(channel);
+        ssh_channel_free(channel);
+        ssh_disconnect(session);
+        ssh_free(session);
+        exit(EXIT_FAILURE);
     }
 
 
@@ -89,78 +89,42 @@ int main(int argc , char *argv[]) {
     printf("\n\n");
     return 0;
 }
+
 /*
-pi@rasp4:~/Coding/c_folder/misc/network_stuff $ ./ssh_client "sbx-nxos-mgmt.cisco.com" "show ip int brief ; show version"
 
-Command: show ip int brief ; show version
+pi@rasp4:~/Coding/c_folder/misc/network_stuff $ ./ssh_client "sbx-nxos-mgmt.cisco.com" "config t ; logging server 1.1.1.1 ; show run | inc logging"
 
-IP Interface Status for VRF "default"(1)
-Interface            IP Address      Interface Status
-Vlan11               192.168.11.1    protocol-down/link-down/admin-down 
-Vlan12               192.168.12.1    protocol-down/link-down/admin-down 
-Vlan13               192.168.14.1    protocol-down/link-down/admin-down 
-Vlan14               192.168.90.1    protocol-down/link-down/admin-up   
-Vlan16               192.168.89.23   protocol-down/link-down/admin-up   
-Vlan25               10.222.249.25   protocol-down/link-down/admin-up   
-Vlan50               172.16.1.111    protocol-down/link-down/admin-down 
-Vlan77               172.16.1.10     protocol-down/link-down/admin-up   
-Vlan88               10.88.88.88     protocol-down/link-down/admin-down 
-Vlan89               10.100.100.89   protocol-down/link-down/admin-down 
-Vlan100              10.0.0.101      protocol-down/link-down/admin-down 
-Vlan112              10.72.240.13    protocol-down/link-down/admin-down 
-Vlan113              10.73.148.13    protocol-down/link-down/admin-down 
-Vlan121              10.0.0.1        protocol-down/link-down/admin-up   
-Vlan204              10.204.204.66   protocol-down/link-down/admin-up   
-Vlan3333             10.1.1.2        protocol-down/link-down/admin-down 
-Vlan3912             1.1.1.1         protocol-down/link-down/admin-down 
-Lo0                  1.1.1.1         protocol-down/link-down/admin-down 
-Lo11                 10.0.0.11       protocol-down/link-down/admin-down 
-Lo55                 55.55.55.55     protocol-up/link-up/admin-up       
-Lo56                 56.56.56.56     protocol-up/link-up/admin-up       
-Lo98                 10.98.98.1      protocol-up/link-up/admin-up       
-Lo99                 10.99.99.1      protocol-up/link-up/admin-up       
-Lo618                159.99.89.99    protocol-up/link-up/admin-up       
-Lo648                159.99.99.99    protocol-up/link-up/admin-up       
-Lo1001               1.1.1.1         protocol-up/link-up/admin-up       
-Eth1/9               1.1.1.1         protocol-down/link-down/admin-down 
-Cisco Nexus Operating System (NX-OS) Software
-TAC support: http://www.cisco.com/tac
-Documents: http://www.cisco.com/en/US/products/ps9372/tsd_products_support_series_home.html
-Copyright (c) 2002-2023, Cisco Systems, Inc. All rights reserved.
-The copyrights to certain works contained herein are owned by
-other third parties and are used and distributed under license.
-Some parts of this software are covered under the GNU Public
-License. A copy of the license is available at
-http://www.gnu.org/licenses/gpl.html.
+Command: config t ; logging server 1.1.1.1 ; show run | inc logging
+ntp logging
+no logging event link-status default
+  logging event port link-status
+  logging event port trunk-status
+  logging event port link-status
+  logging event port trunk-status
+logging server 1.1.1.1
+logging server 10.10.20.175
+logging server 2.2.2.3 7 port 25199
+logging source-interface loopback0
+logging timestamp milliseconds
+logging level syslog 7
+no logging console
 
-Nexus 9000v is a demo version of the Nexus Operating System
 
-Software
-  BIOS: version 
-  NXOS: version 10.3(3) [Feature Release]
-  BIOS compile time:  
-  NXOS image file is: bootflash:///nxos64-cs.10.3.3.F.bin
-  NXOS compile time:  4/30/2023 12:00:00 [05/03/2023 20:18:42]
+pi@rasp4:~/Coding/c_folder/misc/network_stuff $ ./ssh_client "sbx-nxos-mgmt.cisco.com" "config t ; no logging server 1.1.1.1 ; show run | inc logging"
 
-Hardware
-  cisco Nexus9000 C9300v Chassis 
-  Intel(R) Xeon(R) Gold 6148 CPU @ 2.40GHz with 16384716 kB of memory.
-  Processor Board ID 9K1C72YEFLR
-  Device name: Nithish
-  bootflash:    4287040 kB
-
-Kernel uptime is 34 day(s), 15 hour(s), 42 minute(s), 20 second(s)
-
-Last reset 
-  Reason: Unknown
-  System version: 
-  Service: 
-
-plugin
-  Core Plugin, Ethernet Plugin
-
-Active Package(s):
-	
+Command: config t ; no logging server 1.1.1.1 ; show run | inc logging
+ntp logging
+no logging event link-status default
+  logging event port link-status
+  logging event port trunk-status
+  logging event port link-status
+  logging event port trunk-status
+logging server 10.10.20.175
+logging server 2.2.2.3 7 port 25199
+logging source-interface loopback0
+logging timestamp milliseconds
+logging level syslog 7
+no logging console
 
 
 pi@rasp4:~/Coding/c_folder/misc/network_stuff $ 
