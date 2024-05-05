7652ac90c12f0ed5ec7618e740f4ff4dfb65217e
Todd Riemenschneider <twr14152@gmail.com>
2024-05-05 06:08:09 -0400

    Update ssh_client_v2.c

--- network_scripts/ssh_client_v2.c
+++ network_scripts/ssh_client_v2.c
@@ -164,7 +164,7 @@ int display_banner(ssh_session session)
 
 int main(int argc , char *argv[])
 {
-
+    printf("You are running c version %ld\n", __STDC_VERSION__);
     // Initialize SSH variables
     ssh_session session;
     int rc;
@@ -249,4 +249,3 @@ int main(int argc , char *argv[])
     printf("\n\n");
     return 0;
 }
-
