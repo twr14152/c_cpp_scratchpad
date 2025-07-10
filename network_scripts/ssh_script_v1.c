1b3a80e7465d8465cc8c6a4fbc53c97ccfaab2f6
Todd Riemenschneider <twr14152@gmail.com>
2025-07-10 18:07:28 -0400

    Update ssh_script_v1.c
    Cleaned up version tested with Cisco devices nxos, iosxe, ios

--- network_scripts/ssh_script_v1.c
+++ network_scripts/ssh_script_v1.c
@@ -3,10 +3,12 @@
 #include <libssh/libssh.h>
 
 
-int verbosity = SSH_LOG_PROTOCOL;
+int main(int argc, char * argv[]) {
 
-int main(int argc , char *argv[]) {
-      
+  for (int i = 2; i < argc; i++) {
+
+    printf("\nCommand: %s\n", argv[i]);
+
     // Initialize SSH variables
     ssh_session session;
     int rc;
@@ -14,79 +16,78 @@ int main(int argc , char *argv[]) {
     // Create SSH session
     session = ssh_new();
     if (session == NULL) {
-        fprintf(stderr, "Error creating SSH session\n");
-        exit(EXIT_FAILURE);
+      fprintf(stderr, "Error creating SSH session\n");
+      exit(EXIT_FAILURE);
     }
 
     // Set SSH options
-    //ssh_options_set(session, SSH_OPTIONS_LOG_VERBOSITY, &verbosity);
     ssh_options_set(session, SSH_OPTIONS_HOST, argv[1]);
-    ssh_options_set(session, SSH_OPTIONS_USER, "###");
+    ssh_options_set(session, SSH_OPTIONS_USER, "username");   // or you could set value to argv[2] instead of raw string
 
     // Connect to SSH server
     rc = ssh_connect(session);
     if (rc != SSH_OK) {
-        fprintf(stderr, "Error connecting to SSH server: %s\n", ssh_get_error(session));
-        ssh_free(session);
-        exit(EXIT_FAILURE);
+      fprintf(stderr, "Error connecting to SSH server: %s\n", ssh_get_error(session));
+      ssh_free(session);
+      exit(EXIT_FAILURE);
     }
 
     // Authenticate with password
-    rc = ssh_userauth_password(session, NULL, "###");
+    rc = ssh_userauth_password(session, NULL, "password");   // you could also use argv[3] instead raw string
     if (rc != SSH_AUTH_SUCCESS) {
-        fprintf(stderr, "Error authenticating with password: %s\n", ssh_get_error(session));
-        ssh_disconnect(session);
-        ssh_free(session);
-        exit(EXIT_FAILURE);
+      fprintf(stderr, "Error authenticating with password: %s\n", ssh_get_error(session));
+      ssh_disconnect(session);
+      ssh_free(session);
+      exit(EXIT_FAILURE);
     }
 
     // Open channel for executing command
     ssh_channel channel;
     channel = ssh_channel_new(session);
     if (channel == NULL) {
-        fprintf(stderr, "Error creating SSH channel\n");
-        ssh_disconnect(session);
-        ssh_free(session);
-        exit(EXIT_FAILURE);
+      fprintf(stderr, "Error creating SSH channel\n");
+      ssh_disconnect(session);
+      ssh_free(session);
+      exit(EXIT_FAILURE);
     }
 
     // Execute command
     rc = ssh_channel_open_session(channel);
     if (rc != SSH_OK) {
-        fprintf(stderr, "Error opening SSH channel: %s\n", ssh_get_error(session));
-        ssh_channel_free(channel);
-        ssh_disconnect(session);
-        ssh_free(session);
-        exit(EXIT_FAILURE);
-    }
-    
-    printf("\nCommand: %s\n", argv[2]);    
-    rc = ssh_channel_request_exec(channel, argv[2]);
-    if (rc != SSH_OK) {
-	fprintf(stderr, "Error executing command: %s\n", ssh_get_error(session));
-        ssh_channel_close(channel);
-        ssh_channel_free(channel);
-        ssh_disconnect(session);
-        ssh_free(session);
-        exit(EXIT_FAILURE);
+      fprintf(stderr, "Error opening SSH channel: %s\n", ssh_get_error(session));
+      ssh_channel_free(channel);
+      ssh_disconnect(session);
+      ssh_free(session);
+      exit(EXIT_FAILURE);
     }
 
+    //This is where you loop through commands
+    rc = ssh_channel_request_exec(channel, argv[i]);
+    if (rc != SSH_OK) {
+      fprintf(stderr, "Error executing command: %s\n", ssh_get_error(session));
+      ssh_channel_close(channel);
+      ssh_channel_free(channel);
+      ssh_disconnect(session);
+      ssh_free(session);
+      exit(EXIT_FAILURE);
+    }
 
     // Read command output
     char buffer[256];
     int nbytes;
     while ((nbytes = ssh_channel_read(channel, buffer, sizeof(buffer), 0)) > 0) {
-        fwrite(buffer, 1, nbytes, stdout);
+      fwrite(buffer, 1, nbytes, stdout);
     }
 
     // Close channel
     ssh_channel_send_eof(channel);
     ssh_channel_close(channel);
     ssh_channel_free(channel);
+
     // Disconnect and free SSH session
     ssh_disconnect(session);
     ssh_free(session);
-    printf("\n\n");
-    return 0;
+  } //End of loop
+  printf("\n\n");
+  return 0;
 }
-
